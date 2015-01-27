014de8c98c566649ae5ecf5054583e9fb9d8ea84
Atte Virtanen <aten.email@gmail.com>
2015-01-27 02:20:25 +0200

    verify raw squee regex syntax

--- js/rawSquees.js
+++ js/rawSquees.js
@@ -6,11 +6,12 @@ var self = {
 	'window': null,
 	'textarea': null,
 	'button': null,
+	'error': null,
 	'showWindow': function(){
 		self.window = $('body').dialogWindow({
 			'title': 'Raw Squee Management',
 			'uid': 'rawsqueemanagement',
-			'center' :true
+			'center': true
 		});
 
 		self.textarea = $('<textarea>', {
@@ -27,11 +28,36 @@ var self = {
 			'text': 'Save',
 			'click': self.onSaveClick
 		}).appendTo(self.window);
+
+		self.error = $('<pre>', {
+
+		}).appendTo(self.window);
 	},
 	'onSaveClick': function(){
-		HIGHLIGHT_LIST = self.textarea.val().split('\n').filter(function(str){
+		var lines = self.textarea.val().split('\n').filter(function(str){
 			return str.length > 0;
 		});
+
+		var errors = [];
+		lines.forEach(function(line){
+			try{
+				new RegExp(line);
+			}
+			catch(e){
+				errors.push(e);
+			}
+
+			if ( line.indexOf(';') != -1 )
+				errors.push('; is not allowed in squees');
+		});
+
+		self.error.text('');
+		if ( errors.length > 0 ){
+			self.error.text('NOT SAVED, FIX ERRORS:\n' + errors.join('\n'));
+			return;
+		}
+
+		HIGHLIGHT_LIST = lines;
 		localStorage.setItem('highlightList', HIGHLIGHT_LIST.join(';'));
 
 		if ( BerryTweaks.modules.sync )
