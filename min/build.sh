23c347b2cefcd5c8e42329da3f97bdfd6f28ce5a
Atte Virtanen <aten.email@gmail.com>
2015-04-20 19:38:24 +0300

    safer building

--- min/build.sh
+++ min/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-rm -rf css
-cp -r ../css/ .
+rm -rf tmp
+mkdir tmp tmp/js tmp/js/lib
 
-rm -rf js
-mkdir js js/lib
+cp -r ../css/ tmp/
 
-find ../js/ -type f -print0 | sed -z 's|^../||' | xargs -0 -n1 -P4 -i -t \
-	closure --js '../{}' --js_output_file '{}' --create_source_map '{}.map'
+find '../js/' -type f -print0 | sed -z 's|^../||' | xargs -0 -n1 -P4 -i -t \
+	closure --js '../{}' --js_output_file 'tmp/{}' --create_source_map 'tmp/{}.map'
 
-find js -type f -name '*.js' -print0 | xargs -0 -t \
+find 'tmp/js' -type f -name '*.js' -print0 | xargs -0 -t \
 	sed -r --in-place 's#atte.fi/berrytweaks/(css|js)/#atte.fi/berrytweaks/min/\1/#'
+
+rm -rf css js
+mv tmp/{css,js} .
+rm -rf tmp
