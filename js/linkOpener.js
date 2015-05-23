89a99e1bb7ecd803c4a24ad4b997cef9a8867b76
Atte Virtanen <aten.email@gmail.com>
2015-05-24 01:48:36 +0300

    allow skipping spoilers for link auto-opener

--- js/linkOpener.js
+++ js/linkOpener.js
@@ -4,15 +4,40 @@ BerryTweaks.modules['linkOpener'] = (function(){
 var self = {
 	'css': false,
 	'win': null,
+	'hideSpoilers': BerryTweaks.loadSettings().hideSpoilerLinks || false,
 	'handleMessage': function(msg){
-		if ( !self.win || self.win.closed || msg.emote )
+		if ( !self.win || self.win.closed || msg.emote === 'request' )
 			return;
+
+		if ( self.hideSpoilers ){
+			if ( msg.msg.indexOf('spoiler') !== -1 || msg.emote === 'spoiler' )
+				return;
+		}
 
 		var m = msg.msg.match(/\bhttps?:\/\/[^\s]+/i);
 		if ( m )
 			self.win.location.href = m[0];
 	},
 	'addSettings': function(container){
+		$('<div>', {
+
+		}).append(
+			$('<label>', {
+				'for': 'berrytweaks-linkOpener-hideSpoilers',
+				'text': "Don't open spoiler links"
+			})
+		).append(
+			$('<input>', {
+				'id': 'berrytweaks-linkOpener-hideSpoilers',
+				'type': 'checkbox',
+				'checked': self.hideSpoilers
+			}).change(function(){
+				var settings = BerryTweaks.loadSettings();
+				settings.hideSpoilerLinks = self.hideSpoilers = !!$(this).prop('checked');
+				BerryTweaks.saveSettings(settings);
+			})
+		).appendTo(container);
+
 		$('<a>', {
 			'href': 'javascript:void(0)',
 			'click': function(){
