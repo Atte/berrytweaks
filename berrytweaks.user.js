5fe13ea3219a20f3aec1a02412726645e6c76a9b
Atte Virtanen <aten.email@gmail.com>
2016-05-08 19:09:48 +0300

    more matches

--- berrytweaks.user.js
+++ berrytweaks.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         BerryTweaks
 // @namespace    https://atte.fi/berrytweaks/
-// @version      0.3
+// @version      0.4
 // @description  A collection of BerryTube tweaks
 // @author       Atte
-// @match        http://www.berrytube.tv/*
 // @match        http://berrytube.tv/*
+// @match        http://www.berrytube.tv/*
+// @match        http://tunnel.berrypun.ch/*
+// @match        http://tunnel.q-z.xyz/*
+// @match        http://btc.berrytube.tv:8000/*
 // @updateURL    https://atte.fi/berrytweaks/berrytweaks.user.js
+// @downloadURL  https://atte.fi/berrytweaks/berrytweaks.user.js
 // @grant        none
 // ==/UserScript==
 
