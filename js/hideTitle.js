9da6581ae2af77ba75221cd23f6989ef4e62fad7
Atte Virtanen <aten.email@gmail.com>
2017-09-20 12:01:58 +0000

    Make hideTitle hide favicon

--- js/hideTitle.js
+++ js/hideTitle.js
@@ -2,22 +2,36 @@ BerryTweaks.modules['hideTitle'] = (function(){
 'use strict';
 
 const self = {
-    oldTitle: null,
-    oldNotifyTitle: null,
+    old: {
+        title: null,
+        notifyTitle: null
+    },
+    setFavicon(url) {
+        $('link[rel~=icon]').remove();
+        $('<link>', {
+            type: 'image/x-icon',
+            rel: 'shortcut icon',
+            href: url
+        }).appendTo(document.head);
+    },
     enable() {
-        if ( !self.oldTitle )
-            self.oldTitle = window.WINDOW_TITLE;
-        if ( !self.oldNotifyTitle )
-            self.oldNotifyTitle = window.NOTIFY_TITLE;
+        if ( !self.old.title )
+            self.old.title = window.WINDOW_TITLE;
+        if ( !self.old.notifyTitle )
+            self.old.notifyTitle = window.NOTIFY_TITLE;
 
         document.title = window.WINDOW_TITLE = 'BT';
         window.NOTIFY_TITLE = '!';
+
+        self.setFavicon('https://dl.atte.fi/blank.ico');
     },
     disable() {
-        if ( self.oldTitle )
-            document.title = window.WINDOW_TITLE = self.oldTitle;
-        if ( self.oldNotifyTitle )
-            window.NOTIFY_TITLE = self.oldNotifyTitle;
+        if ( self.old.title )
+            document.title = window.WINDOW_TITLE = self.old.title;
+        if ( self.old.notifyTitle )
+            window.NOTIFY_TITLE = self.old.notifyTitle;
+
+        self.setFavicon('/favicon.ico');
     }
 };
 
