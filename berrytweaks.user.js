d1c87c612aabbe7ed71ac50f2be71350482dd898
Atte Virtanen <aten.email@gmail.com>
2018-03-22 21:25:54 +0000

    Preload raven through GreaseMonkey when possible

--- berrytweaks.user.js
+++ berrytweaks.user.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @name         BerryTweaks
 // @namespace    https://atte.fi/berrytweaks/
-// @version      0.5.3
+// @version      0.5.4
 // @description  A collection of BerryTube tweaks
 // @author       Atte
 // @icon         https://dl.atte.fi/bt_favicon.ico
 // @match        http://berrytube.tv/*
 // @match        http://www.berrytube.tv/*
-// @match        http://tunnel.berrypun.ch/*
-// @match        http://tunnel.q-z.xyz/*
 // @match        http://btc.berrytube.tv:8000/*
-// @match        https://bt-proxy.atte.fi/*
 // @downloadURL  https://atte.fi/berrytweaks/berrytweaks.user.js
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/raven.js/3.23.3/raven.min.js#md5=faa0565b124106ce0b4577dbff19ddb0,sha256=7ce91eeae533c4e84634e99babb27d83820ec40084db3a7924be0c6a49db844f
 // @noframes
 // ==/UserScript==
 
--- js/init.js
+++ js/init.js
@@ -1,61 +1,67 @@
 $(function(){
 'use strict';
 
-$.ajax({
-    url: 'https://cdnjs.cloudflare.com/ajax/libs/raven.js/3.23.1/raven.min.js',
-    dataType: 'script',
-    cache: true,
-    success: function() {
-        const interval = setInterval(() => {
-            if (!window.Raven) {
-                return;
-            }
-
-            clearInterval(interval);
-            window.BerryTweaks = {
-                raven: Raven.noConflict(),
-                release: 'RELEASE',
-                releaseUrl: suffix => {
-                    if (BerryTweaks.release) {
-                        return `https://cdn.atte.fi/berrytweaks/${BerryTweaks.release}/${suffix}`;
-                    }
-                    return `https://atte.fi/berrytweaks/${suffix}`;
-                }
-            };
-
-            if (BerryTweaks.release === 'RELEASE') {
-                BerryTweaks.release = null;
-            }
-
-            BerryTweaks.raven.config('https://d709b359cd66469a8fdbd1b1e5d4d8c4@sentry.io/236977', {
-                environment: BerryTweaks.release ? 'production' : 'development',
-                whitelistUrls: [/atte\.fi\/berrytweaks\//],
-                instrument: false,
-                autoBreadcrumbs: {
-                    xhr: true,
-                    console: false,
-                    dom: true,
-                    location: false
-                }
-            }).install();
-
+function initialize() {
+    window.BerryTweaks = {
+        raven: Raven.noConflict(),
+        release: 'RELEASE',
+        releaseUrl: suffix => {
             if (BerryTweaks.release) {
-                BerryTweaks.raven.setRelease(BerryTweaks.release);
+                return `https://cdn.atte.fi/berrytweaks/${BerryTweaks.release}/${suffix}`;
             }
+            return `https://atte.fi/berrytweaks/${suffix}`;
+        }
+    };
 
-            if (window.NAME) {
-                BerryTweaks.raven.setUserContext({
-                    id: window.NAME
-                });
-            }
-
-            $.ajax({
-                url: BerryTweaks.releaseUrl('js/main.js'),
-                dataType: 'script',
-                cache: true
-            });
-        }, 100);
+    if (BerryTweaks.release === 'RELEASE') {
+        BerryTweaks.release = null;
     }
-});
+
+    BerryTweaks.raven.config('https://d709b359cd66469a8fdbd1b1e5d4d8c4@sentry.io/236977', {
+        environment: BerryTweaks.release ? 'production' : 'development',
+        whitelistUrls: [/atte\.fi\/berrytweaks\//],
+        instrument: false,
+        autoBreadcrumbs: {
+            xhr: true,
+            console: false,
+            dom: true,
+            location: false
+        }
+    }).install();
+
+    if (BerryTweaks.release) {
+        BerryTweaks.raven.setRelease(BerryTweaks.release);
+    }
+
+    if (window.NAME) {
+        BerryTweaks.raven.setUserContext({
+            id: window.NAME
+        });
+    }
+
+    $.ajax({
+        url: BerryTweaks.releaseUrl('js/main.js'),
+        dataType: 'script',
+        cache: true
+    });
+}
+
+if (window.Raven) {
+    initialize();
+} else {
+    $.ajax({
+        url: 'https://cdnjs.cloudflare.com/ajax/libs/raven.js/3.23.3/raven.min.js',
+        dataType: 'script',
+        cache: true,
+        success: function() {
+            const interval = setInterval(() => {
+                if (window.Raven) {
+                    clearInterval(interval);
+                    initialize();
+                }
+            }, 100);
+        }
+    });
+}
 
 });
