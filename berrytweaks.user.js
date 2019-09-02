e61a20081f1af379f276024a0e4ac7aeeeb4593d
Atte Virtanen <aten.email@gmail.com>
2019-09-02 19:36:09 +0000

    Remove raven @require

--- berrytweaks.user.js
+++ berrytweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BerryTweaks
 // @namespace    https://atte.fi/berrytweaks/
-// @version      0.5.6
+// @version      0.6.0
 // @description  A collection of BerryTube tweaks
 // @author       Atte
 // @icon         https://atte.fi/berrytweaks/favicon.ico
@@ -9,7 +9,6 @@
 // @match        https://berrytube.tv/*
 // @downloadURL  https://atte.fi/berrytweaks/berrytweaks.user.js
 // @grant        none
-// @require      https://cdnjs.cloudflare.com/ajax/libs/raven.js/3.26.2/raven.min.js#md5=9d93fd0b81d26d806fd3bbda88742eea,sha256=4b0353ddb76fd7318ad95d2600420c21d48484847e1db8581cb324991e94ec3f
 // @noframes
 // ==/UserScript==
 
